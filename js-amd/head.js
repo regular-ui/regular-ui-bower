11bfbe50582269b54cfc1b337876f4c766a9a9ee
Yusen <rainforest92@126.com>
2015-09-21 18:50:34 +0800

    v0.1.3

---
+++ js-amd/head.js
@@ -0,0 +1,15 @@
+define(function (require, exports, module) {/**
+ * ------------------------------------------------------------
+ * RGUI      Regular UI库
+ * @author   sensen(rainforest92@126.com)
+ * ------------------------------------------------------------
+ */
+
+'use strict';
+
+exports.Regular = require('regularjs');
+exports.Component = require('./base/component.js');
+exports.SourceComponent = require('./base/sourceComponent.js');
+exports._ = require('./base/util.js');
+
+});
